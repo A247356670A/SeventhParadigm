8725c02899a97cce8a99c164659fc240ccc99c9a
junxian.chen <a247356670a@gmail.com>
2020-08-25 12:24:04 +1000

    add new detail screen

--- App.js
+++ App.js
@@ -10,64 +10,65 @@ import {createStackNavigator} from '@react-navigation/stack';
 //npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
 
 const HomeScreen = ({navigation}) => {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={styles.red}>Home Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() =>
-                    navigation.navigate('Details')
-                }
-            />
-        </View>
-    );
-}
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={styles.red}>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
 
 const DetailsScreen = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={styles.bigBlue}>Details Screen</Text>
+      <Button title="Go Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go to Details" onPress={() => navigation.navigate('Details2')} />
+    </View>
+  );
+};
+
+const DetailsScreen2 = ({navigation}) => {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text style={styles.bigBlue}>Details Screen</Text>
-            <Button
-                title="Go Home"
-                onPress={() =>
-                    navigation.navigate('Home')
-                }
-            />
+            <Button title="Go Home" onPress={() => navigation.navigate('Home')} />
         </View>
     );
-}
+};
 
 const Stack = createStackNavigator();
 
 const MyStack = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{title: "Welcome"}}
-                />
-                <Stack.Screen
-                    name = "Details"
-                    component = {DetailsScreen}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details2" component={DetailsScreen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 50,
-    },
-    bigBlue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
-    },
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
 });
 export default MyStack;
